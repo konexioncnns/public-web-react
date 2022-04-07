ea074c429bd2dbe218de0b6d61dd8238f708d2df
konexioncnns <67736368+konexioncnns@users.noreply.github.com>
2022-04-07 10:13:23 -0400

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,1 +1,20 @@
 
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Navbar() {
+  return (
+    <div className="App">
+    <div>
+      <li>Acceiul</li>
+     <li>Marque</li>
+     <li>Categories</li>
+     <li>Profile</li>
+    </div>
+ 
+    </div>
+  );
+}
+
+export default Navbar;
